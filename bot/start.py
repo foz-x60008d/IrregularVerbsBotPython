d101b0822a5761d0c9543ba7930d287388867465
akorablev <akorablev@yandex-team.ru>
2018-12-04 22:26:30 +0300

    минимальная работа с тренировками

--- bot/start.py
+++ bot/start.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
+from typing import Dict
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+from bot.classes import TrainSession, Word
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -10,8 +12,50 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+# текущие упражнения по id чата
+train_sessions_db: Dict[int, TrainSession] = dict()
+
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Будем тренироваться?\nНабери /train для начала тренировки"
+    )
+
+
+def train(bot, update):
+    if update.message.chat_id not in train_sessions_db:
+        train_sessions_db[update.message.chat_id] = TrainSession(
+            [Word('do', 'did', 'done'), Word('set', 'set', 'set')]
+        )
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=train_sessions_db[update.message.chat_id].ask()
+    )
+
+
+def answer(bot, update):
+    if update.message.chat_id not in train_sessions_db:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Будем тренироваться?\nНабери /train для начала тренировки"
+        )
+        return
+
+    result = train_sessions_db[update.message.chat_id].check_answer(update.message.text)
+    message = "Все верно!" if result else "Не верно!"
+
+    if train_sessions_db[update.message.chat_id].finished:
+        message += "\nТренировка завершена."
+        del train_sessions_db[update.message.chat_id]
+    else:
+        message += "\n" + train_sessions_db[update.message.chat_id].ask()
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=message
+    )
 
 
 def error(bot, update, error):
@@ -30,11 +74,15 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
+    train_handler = CommandHandler('train', train)
+    dispatcher.add_handler(train_handler)
+
+    answer_handler = MessageHandler(Filters.text, answer)
+    dispatcher.add_handler(answer_handler)
+
     dispatcher.add_error_handler(error)
 
     updater.start_polling()
     updater.idle()
 
-
-if __name__ == '__main__':
-    main()
+main()
